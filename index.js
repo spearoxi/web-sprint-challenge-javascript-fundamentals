8a83ba96bde17c9b79806152e098eac361d14402
David Spear <davidspear@Davids-MacBook-Pro.local>
2021-06-18 11:59:33 -0400

    finished task 2

--- index.js
+++ index.js
@@ -61,10 +61,15 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(array){
+    const displayNames = [];
+
+    array.forEach(function(item){
+        return displayNames.push(`name: ${item.animal_name}, scientific: ${item.scientific_name}`);
+    });
+      return displayNames;
   }
-  
+  console.log(animalNames(zooAnimals));
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoo needs a list of all their animal's names converted to lower case. 
